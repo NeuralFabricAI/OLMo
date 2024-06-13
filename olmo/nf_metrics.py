34222cb4ef312a7a9c37d87310b6331ef4439bfa
Leon Xu <lx@neuralfabric.ai>
2024-06-13 00:38:22 -0700

    more log

--- olmo/nf_metrics.py
+++ olmo/nf_metrics.py
@@ -24,9 +24,9 @@ class NFMetrics:
         if checkpoint_exists:
             try:
                 s3 = boto3.client('s3')
-                log.info(f"Downloading metrics file from S3: {bucket}/{filename}")
+                log.info(f"Downloading metrics file from S3: {bucket}/{filename} to local file {self.metrics_file}")
                 s3.download_file(bucket, filename, self.metrics_file)
-                log.info(f"Downloaded metrics file from S3: {bucket}/{filename}")
+                log.info(f"Downloaded metrics file from S3: {bucket}/{filename} to local file {self.metrics_file}")
             except Exception as e:
                 log.error(f"Error downloading metrics file from S3: {e}.")
 
